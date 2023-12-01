09fdb1b51c11f81136bd36a6bd312587f7c0c737
isnaayu_m <isnaayu2@gmail.com>
2023-12-01 16:32:15 +0700

    [FEATURE] - dependensi

---
+++ src/main/java/com/enigma/util/Direction.java
@@ -0,0 +1,21 @@
+package com.enigma.util;
+
+public enum Direction {
+    N,
+    E,
+    S,
+    W;
+
+    private String direction;
+    public String getDirection(){
+        return direction;
+    }
+
+
+    public Direction turnRight(){
+        return values()[(this.ordinal()+1)% values().length ];
+    }
+    public Direction turnLeft(){
+        return values()[(this.ordinal()-1) % values().length ];
+    }
+}
